ff5f7b36c3d1b7067179ae87f568a61f02813dab
Kiefer <kiefersmith@outlook.ie>
2022-11-17 23:53:16 +0000

    Added description to records

--- add_record_form.php
+++ add_record_form.php
@@ -32,7 +32,11 @@ include('includes/header.php');
 
             <label>List Price:</label>
             <input type="input" name="price">
-            <br>        
+            <br>    
+            
+            <label>Description:</label>
+            <input type="input" name="Description">
+            <br>      
             
             <label>Image:</label>
             <input type="file" name="image" accept="image/*" />
