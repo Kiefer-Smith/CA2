a857bed5903e9e8b043463418f81cee44583011c
Kiefer <kiefersmith@outlook.ie>
2022-11-18 00:53:31 +0000

    record_id changed to product_id

--- delete_product.php
+++ delete_product.php
@@ -2,15 +2,15 @@
 require_once('database.php');
 
 // Get IDs
-$product_id = filter_input(INPUT_POST, 'record_id', FILTER_VALIDATE_INT);
+$product_id = filter_input(INPUT_POST, 'product_id', FILTER_VALIDATE_INT);
 $category_id = filter_input(INPUT_POST, 'category_id', FILTER_VALIDATE_INT);
 
 // Delete the product from the database
 if ($product_id != false && $category_id != false) {
     $query = "DELETE FROM Products
-              WHERE recordID = :record_id";
+              WHERE productID = :product_id";
     $statement = $db->prepare($query);
-    $statement->bindValue(':record_id', $product_id);
+    $statement->bindValue(':product_id', $product_id);
     $statement->execute();
     $statement->closeCursor();
 }
