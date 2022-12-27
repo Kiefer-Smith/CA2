b3628ac75b6a99f4f924e4ad85f07b799d668825
Kiefer <kiefersmith@outlook.ie>
2022-12-27 01:12:52 +0000

    Added html for navbar

--- includes/header.php
+++ includes/header.php
@@ -7,3 +7,17 @@
 <!-- the body section -->
 <body>
 <header><h1>My PHP CRUD app</h1></header>
+<div class="navbar">
+  <a href="#home">Home</a>
+  <a href="#news">News</a>
+  <div class="dropdown">
+    <button class="dropbtn">Dropdown
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+      <a href="#">Link 1</a>
+      <a href="#">Link 2</a>
+      <a href="#">Link 3</a>
+    </div>
+  </div>
+</div>
