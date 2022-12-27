d31b1e2e3593c0aed5994103362959275d421436
Kiefer <kiefersmith@outlook.ie>
2022-12-27 02:34:49 +0000

    Fixed navbar

--- includes/header.php
+++ includes/header.php
@@ -12,7 +12,7 @@
 <img src="image_uploads/logo.png" alt="logo" />
 <header><h1>PC Station</h1></header></div>
 <div class="navbar">
-  <a class="active" href="index.php">Home</a>
+  <a class="active" href="http://localhost/CA2/?category_id=18">Home</a>
   <a href="add_record_form.php">Add record</a>
   <a href="category_list.php">Add category</a>
   <div class="dropdown">
@@ -20,9 +20,11 @@
       <i class="fa fa-caret-down"></i>
     </button>
     <div class="dropdown-content">
-      <a href="#">Office PCs</a>
-      <a href="#">Gaming PCs</a>
-      <a href="#">Laptops</a>
+      <a href="http://localhost/CA2/?category_id=18">Office PCs</a>
+      <a href="http://localhost/CA2/?category_id=19">Gaming PCs</a>
+      <a href="http://localhost/CA2/?category_id=20">Laptops</a>
     </div>
   </div>
+  <a href="http://localhost/CA2/?category_id=21">PC Deals</a>
+  <a href="http://localhost/CA2/?category_id=22">PC Components</a>
 </div>
