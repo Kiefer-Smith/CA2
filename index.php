f4ea192b87a41a4f18ec594c5f2ff02a79ee746e
Kiefer <kiefersmith@outlook.ie>
2023-01-01 21:31:23 +0000

    Added Products

--- index.php
+++ index.php
@@ -64,7 +64,8 @@ include('includes/header.php');
             <span class="float-end"><a href="#" class="small text-muted text-uppercase aff-link">reviews</a></span>
           </div>
           <h5 class="card-title">
-            <a target="_blank" href="#">AURORA SUPREME Intel® Core™ i3-10100
+            <a target="_blank" href="#">AURORA SUPREME
+                 Intel® Core™ i3-10100
             ASUS® PRIME H510M-A
             8GB Corsair VENGEANCE 3200MHz
             2GB GEFORCE GT 1030 - DVI, HDMI
@@ -634,7 +635,7 @@ Windows Hello</a>
     <div class="col hp">
       <div class="card h-100 shadow-sm">
         <a href="#">
-          <img src="image_uploads/pulsar elite.png" class="card-img-top" alt="product.title" />
+          <img src="image_uploads/3.png" class="card-img-top" alt="product.title" />
         </a>
  
         <div class="label-top shadow-sm">
@@ -642,19 +643,32 @@ Windows Hello</a>
         </div>
         <div class="card-body">
           <div class="clearfix mb-3">
-            <span class="float-start badge rounded-pill bg-success">€2300</span>
+            <span class="float-start badge rounded-pill bg-success">Was €2300 now €1900</span>
  
             <span class="float-end"><a href="#" class="small text-muted text-uppercase aff-link">reviews</a></span>
           </div>
           <h5 class="card-title">
             <a target="_blank" href="#">
-                PULSAR ELITE
-                Intel® Core™ i5-12400F
-ASUS® TUF GAMING Z690-PLUS WIFI D4
-8GB Corsair VENGEANCE 3200MHz
-8GB NVIDIA GEFORCE RTX 3070
-250GB SAMSUNG 970 EVO PLUS M.2
-1TB SEAGATE BARRACUDA HDD</a>
+            XPS Desktop
+            <p style="color:red">*SALE*</p>
+            Processor
+
+12th Gen Intel® Core™ i7-12700 (25 MB cache, 12 cores, 20 threads, 2.10 GHz to 4.90 GHz Turbo)
+Operating System
+Windows 11 Home, English
+Graphics Card
+AMD® Radeon™ RX 6700 XT, 12 GB GDDR6
+Hard Drive
+256 GB, M.2, PCIe NVMe, SSD
+Memoryi
+8 GB, 1 x 8 GB, DDR5, 4400 MHz; up to 128 GB (add'l mem sold separately)
+Wirelessi
+Intel® Killer Wi-Fi 6E (6 where 6E unavailable) AX 1675, 2x2, 802.11ax, MU-MIMO, Bluetooth 5.2
+Microsoft Office
+No Microsoft Office Included
+Security Software
+McAfee LiveSafe 12 Month Subscription
+        </a>
           </h5>
  
           <div class="d-grid gap-2 my-4">
@@ -677,27 +691,38 @@ ASUS® TUF GAMING Z690-PLUS WIFI D4
     <div class="col hp">
       <div class="card h-100 shadow-sm">
         <a href="#">
-          <img src="image_uploads/cobalt pro.png" class="card-img-top" alt="product.title" />
+          <img src="image_uploads/5.png" class="card-img-top" alt="product.title" />
         </a>
  
         <div class="label-top shadow-sm">
-          <a class="text-white" href="#">Intel</a>
+          <a class="text-white" href="#">AMD</a>
         </div>
         <div class="card-body">
           <div class="clearfix mb-3">
-            <span class="float-start badge rounded-pill bg-success">€2000</span>
+            <span class="float-start badge rounded-pill bg-success">Was €2000 now €1500</span>
  
             <span class="float-end"><a href="#" class="small text-muted text-uppercase aff-link">reviews</a></span>
           </div>
           <h5 class="card-title">
             <a target="_blank" href="#">
-                COBALT PRO
-            Intel® Core™ i7-12700KF
-ASUS® TUF GAMING Z690-PLUS WIFI D4
-16GB Corsair VENGEANCE RGB PRO 3600MHz
-12GB AMD RADEON™ RX 6700 XT
-128GB PCS PCIe M.2 SSD
-2TB SEAGATE BARRACUDA HDD</a>
+            Alienware Aurora Ryzen™ Edition R14 Gaming Desktop
+                <p style="color:red">*SALE*</p>
+                Processor
+AMD® Ryzen™ 7 5800 (36 MB total cache, 8 cores, 16 threads, up to 4.60 GHz Max Boost Clock)
+Operating System
+Windows 11 Home, English
+Graphics Card
+NVIDIA® GeForce RTX™ 3070 Ti, 8 GB GDDR6X
+Hard Drive
+256GB NVMe M.2 PCIe SSD (Boot) + 1TB 7200RPM SATA 6Gb/s (Storage)
+Memoryi
+16 GB, 2 x 8 GB, DDR4, 3200 MHz, XMP
+Wirelessi
+Realtek RTL 8822CE Wi-Fi 5 (2x2) MIMO 802.11ac Wireless LAN and Bluetooth 5.0
+Microsoft Office
+No Microsoft Office Included
+Security Software
+No Anti-virus Requested</a>
           </h5>
  
           <div class="d-grid gap-2 my-4">
@@ -720,7 +745,7 @@ ASUS® TUF GAMING Z690-PLUS WIFI D4
     <div class="col hp">
       <div class="card h-100 shadow-sm">
         <a href="#">
-          <img src="image_uploads/cobalt ultra.png" class="card-img-top" alt="product.title" />
+          <img src="image_uploads/6.png" class="card-img-top" alt="product.title" />
         </a>
  
         <div class="label-top shadow-sm">
@@ -728,21 +753,31 @@ ASUS® TUF GAMING Z690-PLUS WIFI D4
         </div>
         <div class="card-body">
           <div class="clearfix mb-3">
-            <span class="float-start badge rounded-pill bg-success">€2550</span>
+            <span class="float-start badge rounded-pill bg-success">Was €1229 now €1000 </span>
  
             <span class="float-end"><a href="#" class="small text-muted text-uppercase aff-link">reviews</a></span>
           </div>
           <h5 class="card-title">
             <a target="_blank" href="#">
 
-            COBALT ULTRA
-
-Intel® Core™ i9-12900K
-ASUS® TUF GAMING Z690-PLUS WIFI D4
-16GB Corsair VENGEANCE RGB PRO 3600MHz
-10GB NVIDIA GEFORCE RTX 3080
-512GB PCS PCIe M.2 SSD
-2TB SEAGATE BARRACUDA HDD</a>
+            Inspiron 27 All-In-One
+            <p style="color:red">*SALE*</p>
+            Processor
+12th Gen Intel® Core™ i7-1255U (12 MB cache, 10 cores, 12 threads, up to 4.70 GHz Turbo)
+Operating System
+Windows 11 Home, English
+Graphics Card
+NVIDIA® GeForce® MX550, 2 GB GDDR6
+Hard Drive
+512GB Solid State Drive (Boot) + 1TB 5400RPM Hard Drive (Storage)
+Memoryi
+16GB, 2x8GB, DDR4, 3200MHz
+Wirelessi
+Intel® Wi-Fi 6E (6GHz) AX211 2x2 Bluetooth 5.2 Wireless Card
+Monitor
+27", FHD 1920x1080, 60Hz, Touch, InfinityEdge
+Case
+Touch Pearl White</a>
           </h5>
  
           <div class="d-grid gap-2 my-4">
@@ -765,7 +800,7 @@ ASUS® TUF GAMING Z690-PLUS WIFI D4
     <div class="col hp">
       <div class="card h-100 shadow-sm">
         <a href="#">
-          <img src="image_uploads/pulsar ultra.png" class="card-img-top" alt="product.title" />
+          <img src="image_uploads/7.png" class="card-img-top" alt="product.title" />
         </a>
         <!-- <div class="label-top shadow-sm">Asus Rog</div>  -->
         <div class="label-top shadow-sm">
@@ -779,14 +814,34 @@ ASUS® TUF GAMING Z690-PLUS WIFI D4
           </div>
           <h5 class="card-title">
             <a target="_blank" href="#">
-PULSAR ULTRA
+            OMEN by HP 25L Gaming Desktop
+<p style="color:red">*SALE*</p>
 
-Intel® Core™ i7-12700KF
-ASUS® TUF GAMING Z690-PLUS WIFI D4
-16GB Corsair VENGEANCE 3200MHz
-24GB NVIDIA GEFORCE RTX 4090
-512GB PCS PCIe M.2 SSD
-1TB SEAGATE BARRACUDA HDD</a>
+Operating system
+
+
+Windows 11 Home
+
+Processor
+
+AMD Ryzen™ 7 5700G (up to 4.6 GHz max boost clock, 16 MB L3 cache, 8 cores, 16 threads)[6,7]
+
+Graphics
+
+Integrated: AMD Radeon™ Graphics
+Discrete: AMD Radeon™ RX 6700 XT Graphics (12 GB GDDR6 dedicated)
+
+Memory
+
+HyperX 16 GB DDR4-3200 MHz XMP RGB Heatsink RAM (2 x 8 GB)
+
+Memory slots
+
+4 DIMM
+
+Storage
+
+512 GB WD Black PCIe® NVMe™ TLC M.2 SSD</a>
           </h5>
  
           <div class="d-grid gap-2 my-4">
