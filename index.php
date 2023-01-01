c7bac2a03d6d602bf5a0e88bf7afc2a01854d488
Kiefer <kiefersmith@outlook.ie>
2023-01-01 20:07:57 +0000

    Edited body layout

--- index.php
+++ index.php
@@ -55,7 +55,7 @@ include('includes/header.php');
         </a>
  
         <div class="label-top shadow-sm">
-          <a class="text-white" href="#">asus</a>
+          <a class="text-white" href="#">Intel</a>
         </div>
         <div class="card-body">
           <div class="clearfix mb-3">
@@ -96,16 +96,24 @@ include('includes/header.php');
         </a>
  
         <div class="label-top shadow-sm">
-          <a class="text-white" href="#">razer</a>
+          <a class="text-white" href="#">Intel</a>
         </div>
         <div class="card-body">
           <div class="clearfix mb-3">
-            <span class="float-start badge rounded-pill bg-success">2.345$</span>
+            <span class="float-start badge rounded-pill bg-success">€650</span>
  
             <span class="float-end"><a href="#" class="small text-muted text-uppercase aff-link">reviews</a></span>
           </div>
           <h5 class="card-title">
-            <a target="_blank" href="#">Razer Blade 15 Base Gaming Laptop 2020: Intel Core i7-10750H 6-Core, NVIDIA GeForce GTX 1660 Ti, 15.6" FHD 1080p 120Hz, 16GB RAM, 256GB SSD, CNC Aluminum, Chroma RGB Lighting, Black</a>
+            <a target="_blank" href="#">
+            DIMENSION SPARK
+
+            Intel® Core™ i3-10100
+            GIGABYTE H510M H
+            8GB Corsair VENGEANCE 3200MHz
+            2GB GEFORCE GT 1030 - DVI, HDMI
+            256GB PCS PCIe M.2 SSD
+            256GB PCS 2.5" SSD</a>
           </h5>
  
           <div class="d-grid gap-2 my-4">
@@ -128,20 +136,28 @@ include('includes/header.php');
     <div class="col hp">
       <div class="card h-100 shadow-sm">
         <a href="#">
-          <img src="https://m.media-amazon.com/images/I/81w+3k4U8PL._AC_SL1500_.jpg" class="card-img-top" alt="product.title" />
+          <img src="image_uploads/Fusion Goliath.png" class="card-img-top" alt="product.title" />
         </a>
  
         <div class="label-top shadow-sm">
-          <a class="text-white" href="#">lenovo</a>
+          <a class="text-white" href="#">AMD</a>
         </div>
         <div class="card-body">
           <div class="clearfix mb-3">
-            <span class="float-start badge rounded-pill bg-success">1.020$</span>
+            <span class="float-start badge rounded-pill bg-success">€950</span>
  
             <span class="float-end"><a href="#" class="small text-muted text-uppercase aff-link">reviews</a></span>
           </div>
           <h5 class="card-title">
-            <a target="_blank" href="#">Lenovo Legion 5 Gaming Laptop, 15.6" FHD (1920x1080) IPS Screen, AMD Ryzen 7 4800H Processor, 16GB DDR4, 512GB SSD, NVIDIA GTX 1660Ti, Windows 10, 82B1000AUS, Phantom Black</a>
+            <a target="_blank" href="#">
+FUSION GOLIATH
+
+AMD Ryzen 9 7950X
+ASUS® TUF GAMING X670E-PLUS WIFI
+32GB Corsair VENGEANCE 4800MHz
+24GB AMD RADEON™ RX 7900 XTX
+1TB SEAGATE FIRECUDA 530 GEN 4
+Windows 11 Home</a>
           </h5>
  
           <div class="d-grid gap-2 my-4">
@@ -164,20 +180,27 @@ include('includes/header.php');
     <div class="col hp">
       <div class="card h-100 shadow-sm">
         <a href="#">
-          <img src="https://m.media-amazon.com/images/I/61Ze2wc9nyS._AC_SL1500_.jpg" class="card-img-top" alt="product.title" />
+          <img src="image_uploads/aurora ultra.png" class="card-img-top" alt="product.title" />
         </a>
         <!-- <div class="label-top shadow-sm">Asus Rog</div>  -->
         <div class="label-top shadow-sm">
-          <a class="text-white" href="#">msi</a>
+          <a class="text-white" href="#">Intel</a>
         </div>
         <div class="card-body">
           <div class="clearfix mb-3">
-            <span class="float-start badge rounded-pill bg-success">2.245$</span>
+            <span class="float-start badge rounded-pill bg-success">€850</span>
  
             <span class="float-end"><a href="#" class="small text-muted text-uppercase aff-link">reviews</a></span>
           </div>
           <h5 class="card-title">
-            <a target="_blank" href="#">MSI GL66 Gaming Laptop: 15.6" 144Hz FHD 1080p Display, Intel Core i7-11800H, NVIDIA GeForce RTX 3070, 16GB, 512GB SSD, Win10, Black (11UGK-001)</a>
+            <a target="_blank" href="#">AURORA ULTRA
+
+Intel® Core™ i3-10100
+GIGABYTE H510M H
+8GB Corsair VENGEANCE 3200MHz
+2GB GEFORCE GT 1030 - DVI, HDMI
+512GB SOLIDIGM P41+ GEN 4 M.2
+2TB SEAGATE BARRACUDA HDD</a>
           </h5>
  
           <div class="d-grid gap-2 my-4">
@@ -185,6 +208,7 @@ include('includes/header.php');
             <a href="#" class="btn btn-warning bold-btn">add to cart</a>
  
           </div>
+          
           <div class="clearfix mb-1">
  
             <span class="float-start"><a href="#"><i class="fas fa-question-circle"></i></a></span>
@@ -202,8 +226,375 @@ include('includes/header.php');
 </div>
  
 
-    
+<h2 style="text-align:center">Gaming PCs</h2>
 
+
+<div class="container-fluid bg-trasparent my-4 p-3" style="position: relative">
+  <div class="row row-cols-1 row-cols-xs-2 row-cols-sm-2 row-cols-lg-4 g-3">
+    <div class="col hp">
+      <div class="card h-100 shadow-sm">
+        <a href="#">
+          <img src="image_uploads/pulsar elite.png" class="card-img-top" alt="product.title" />
+        </a>
+ 
+        <div class="label-top shadow-sm">
+          <a class="text-white" href="#">Intel</a>
+        </div>
+        <div class="card-body">
+          <div class="clearfix mb-3">
+            <span class="float-start badge rounded-pill bg-success">€2300</span>
+ 
+            <span class="float-end"><a href="#" class="small text-muted text-uppercase aff-link">reviews</a></span>
+          </div>
+          <h5 class="card-title">
+            <a target="_blank" href="#">
+                PULSAR ELITE
+                Intel® Core™ i5-12400F
+ASUS® TUF GAMING Z690-PLUS WIFI D4
+8GB Corsair VENGEANCE 3200MHz
+8GB NVIDIA GEFORCE RTX 3070
+250GB SAMSUNG 970 EVO PLUS M.2
+1TB SEAGATE BARRACUDA HDD</a>
+          </h5>
+ 
+          <div class="d-grid gap-2 my-4">
+ 
+            <a href="#" class="btn btn-warning bold-btn">add to cart</a>
+ 
+          </div>
+          <div class="clearfix mb-1">
+ 
+            <span class="float-start"><a href="#"><i class="fas fa-question-circle"></i></a></span>
+ 
+            <span class="float-end">
+              <i class="far fa-heart" style="cursor: pointer"></i>
+ 
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col hp">
+      <div class="card h-100 shadow-sm">
+        <a href="#">
+          <img src="image_uploads/cobalt pro.png" class="card-img-top" alt="product.title" />
+        </a>
+ 
+        <div class="label-top shadow-sm">
+          <a class="text-white" href="#">Intel</a>
+        </div>
+        <div class="card-body">
+          <div class="clearfix mb-3">
+            <span class="float-start badge rounded-pill bg-success">€2000</span>
+ 
+            <span class="float-end"><a href="#" class="small text-muted text-uppercase aff-link">reviews</a></span>
+          </div>
+          <h5 class="card-title">
+            <a target="_blank" href="#">
+                COBALT PRO
+            Intel® Core™ i7-12700KF
+ASUS® TUF GAMING Z690-PLUS WIFI D4
+16GB Corsair VENGEANCE RGB PRO 3600MHz
+12GB AMD RADEON™ RX 6700 XT
+128GB PCS PCIe M.2 SSD
+2TB SEAGATE BARRACUDA HDD</a>
+          </h5>
+ 
+          <div class="d-grid gap-2 my-4">
+ 
+            <a href="#" class="btn btn-warning bold-btn">add to cart</a>
+ 
+          </div>
+          <div class="clearfix mb-1">
+ 
+            <span class="float-start"><a href="#"><i class="fas fa-question-circle"></i></a></span>
+ 
+            <span class="float-end">
+              <i class="far fa-heart" style="cursor: pointer"></i>
+ 
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col hp">
+      <div class="card h-100 shadow-sm">
+        <a href="#">
+          <img src="image_uploads/cobalt ultra.png" class="card-img-top" alt="product.title" />
+        </a>
+ 
+        <div class="label-top shadow-sm">
+          <a class="text-white" href="#">Intel</a>
+        </div>
+        <div class="card-body">
+          <div class="clearfix mb-3">
+            <span class="float-start badge rounded-pill bg-success">€2550</span>
+ 
+            <span class="float-end"><a href="#" class="small text-muted text-uppercase aff-link">reviews</a></span>
+          </div>
+          <h5 class="card-title">
+            <a target="_blank" href="#">
+
+            COBALT ULTRA
+
+Intel® Core™ i9-12900K
+ASUS® TUF GAMING Z690-PLUS WIFI D4
+16GB Corsair VENGEANCE RGB PRO 3600MHz
+10GB NVIDIA GEFORCE RTX 3080
+512GB PCS PCIe M.2 SSD
+2TB SEAGATE BARRACUDA HDD</a>
+          </h5>
+ 
+          <div class="d-grid gap-2 my-4">
+ 
+            <a href="#" class="btn btn-warning bold-btn">add to cart</a>
+ 
+          </div>
+          <div class="clearfix mb-1">
+ 
+            <span class="float-start"><a href="#"><i class="fas fa-question-circle"></i></a></span>
+ 
+            <span class="float-end">
+              <i class="far fa-heart" style="cursor: pointer"></i>
+ 
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col hp">
+      <div class="card h-100 shadow-sm">
+        <a href="#">
+          <img src="image_uploads/pulsar ultra.png" class="card-img-top" alt="product.title" />
+        </a>
+        <!-- <div class="label-top shadow-sm">Asus Rog</div>  -->
+        <div class="label-top shadow-sm">
+          <a class="text-white" href="#">Intel</a>
+        </div>
+        <div class="card-body">
+          <div class="clearfix mb-3">
+            <span class="float-start badge rounded-pill bg-success">€3310</span>
+ 
+            <span class="float-end"><a href="#" class="small text-muted text-uppercase aff-link">reviews</a></span>
+          </div>
+          <h5 class="card-title">
+            <a target="_blank" href="#">
+PULSAR ULTRA
+
+Intel® Core™ i7-12700KF
+ASUS® TUF GAMING Z690-PLUS WIFI D4
+16GB Corsair VENGEANCE 3200MHz
+24GB NVIDIA GEFORCE RTX 4090
+512GB PCS PCIe M.2 SSD
+1TB SEAGATE BARRACUDA HDD</a>
+          </h5>
+ 
+          <div class="d-grid gap-2 my-4">
+ 
+            <a href="#" class="btn btn-warning bold-btn">add to cart</a>
+ 
+          </div>
+          
+          <div class="clearfix mb-1">
+ 
+            <span class="float-start"><a href="#"><i class="fas fa-question-circle"></i></a></span>
+ 
+            <span class="float-end">
+              
+<i class="far fa-heart" style="cursor: pointer"></i>
+ 
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+ 
+<h3 style="text-align:center">Laptops</h3>
+
+
+<div class="container-fluid bg-trasparent my-4 p-3" style="position: relative">
+  <div class="row row-cols-1 row-cols-xs-2 row-cols-sm-2 row-cols-lg-4 g-3">
+    <div class="col hp">
+      <div class="card h-100 shadow-sm">
+        <a href="#">
+          <img src="image_uploads/pulsar elite.png" class="card-img-top" alt="product.title" />
+        </a>
+ 
+        <div class="label-top shadow-sm">
+          <a class="text-white" href="#">Intel</a>
+        </div>
+        <div class="card-body">
+          <div class="clearfix mb-3">
+            <span class="float-start badge rounded-pill bg-success">€2300</span>
+ 
+            <span class="float-end"><a href="#" class="small text-muted text-uppercase aff-link">reviews</a></span>
+          </div>
+          <h5 class="card-title">
+            <a target="_blank" href="#">
+                PULSAR ELITE
+                Intel® Core™ i5-12400F
+ASUS® TUF GAMING Z690-PLUS WIFI D4
+8GB Corsair VENGEANCE 3200MHz
+8GB NVIDIA GEFORCE RTX 3070
+250GB SAMSUNG 970 EVO PLUS M.2
+1TB SEAGATE BARRACUDA HDD</a>
+          </h5>
+ 
+          <div class="d-grid gap-2 my-4">
+ 
+            <a href="#" class="btn btn-warning bold-btn">add to cart</a>
+ 
+          </div>
+          <div class="clearfix mb-1">
+ 
+            <span class="float-start"><a href="#"><i class="fas fa-question-circle"></i></a></span>
+ 
+            <span class="float-end">
+              <i class="far fa-heart" style="cursor: pointer"></i>
+ 
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col hp">
+      <div class="card h-100 shadow-sm">
+        <a href="#">
+          <img src="image_uploads/cobalt pro.png" class="card-img-top" alt="product.title" />
+        </a>
+ 
+        <div class="label-top shadow-sm">
+          <a class="text-white" href="#">Intel</a>
+        </div>
+        <div class="card-body">
+          <div class="clearfix mb-3">
+            <span class="float-start badge rounded-pill bg-success">€2000</span>
+ 
+            <span class="float-end"><a href="#" class="small text-muted text-uppercase aff-link">reviews</a></span>
+          </div>
+          <h5 class="card-title">
+            <a target="_blank" href="#">
+                COBALT PRO
+            Intel® Core™ i7-12700KF
+ASUS® TUF GAMING Z690-PLUS WIFI D4
+16GB Corsair VENGEANCE RGB PRO 3600MHz
+12GB AMD RADEON™ RX 6700 XT
+128GB PCS PCIe M.2 SSD
+2TB SEAGATE BARRACUDA HDD</a>
+          </h5>
+ 
+          <div class="d-grid gap-2 my-4">
+ 
+            <a href="#" class="btn btn-warning bold-btn">add to cart</a>
+ 
+          </div>
+          <div class="clearfix mb-1">
+ 
+            <span class="float-start"><a href="#"><i class="fas fa-question-circle"></i></a></span>
+ 
+            <span class="float-end">
+              <i class="far fa-heart" style="cursor: pointer"></i>
+ 
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col hp">
+      <div class="card h-100 shadow-sm">
+        <a href="#">
+          <img src="image_uploads/cobalt ultra.png" class="card-img-top" alt="product.title" />
+        </a>
+ 
+        <div class="label-top shadow-sm">
+          <a class="text-white" href="#">Intel</a>
+        </div>
+        <div class="card-body">
+          <div class="clearfix mb-3">
+            <span class="float-start badge rounded-pill bg-success">€2550</span>
+ 
+            <span class="float-end"><a href="#" class="small text-muted text-uppercase aff-link">reviews</a></span>
+          </div>
+          <h5 class="card-title">
+            <a target="_blank" href="#">
+
+            COBALT ULTRA
+
+Intel® Core™ i9-12900K
+ASUS® TUF GAMING Z690-PLUS WIFI D4
+16GB Corsair VENGEANCE RGB PRO 3600MHz
+10GB NVIDIA GEFORCE RTX 3080
+512GB PCS PCIe M.2 SSD
+2TB SEAGATE BARRACUDA HDD</a>
+          </h5>
+ 
+          <div class="d-grid gap-2 my-4">
+ 
+            <a href="#" class="btn btn-warning bold-btn">add to cart</a>
+ 
+          </div>
+          <div class="clearfix mb-1">
+ 
+            <span class="float-start"><a href="#"><i class="fas fa-question-circle"></i></a></span>
+ 
+            <span class="float-end">
+              <i class="far fa-heart" style="cursor: pointer"></i>
+ 
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col hp">
+      <div class="card h-100 shadow-sm">
+        <a href="#">
+          <img src="image_uploads/pulsar ultra.png" class="card-img-top" alt="product.title" />
+        </a>
+        <!-- <div class="label-top shadow-sm">Asus Rog</div>  -->
+        <div class="label-top shadow-sm">
+          <a class="text-white" href="#">Intel</a>
+        </div>
+        <div class="card-body">
+          <div class="clearfix mb-3">
+            <span class="float-start badge rounded-pill bg-success">€3310</span>
+ 
+            <span class="float-end"><a href="#" class="small text-muted text-uppercase aff-link">reviews</a></span>
+          </div>
+          <h5 class="card-title">
+            <a target="_blank" href="#">
+PULSAR ULTRA
+
+Intel® Core™ i7-12700KF
+ASUS® TUF GAMING Z690-PLUS WIFI D4
+16GB Corsair VENGEANCE 3200MHz
+24GB NVIDIA GEFORCE RTX 4090
+512GB PCS PCIe M.2 SSD
+1TB SEAGATE BARRACUDA HDD</a>
+          </h5>
+ 
+          <div class="d-grid gap-2 my-4">
+ 
+            <a href="#" class="btn btn-warning bold-btn">add to cart</a>
+ 
+          </div>
+          
+          <div class="clearfix mb-1">
+ 
+            <span class="float-start"><a href="#"><i class="fas fa-question-circle"></i></a></span>
+ 
+            <span class="float-end">
+              
+<i class="far fa-heart" style="cursor: pointer"></i>
+ 
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 
 <!-- <section> -->
 <!-- display a table of Products -->
@@ -246,7 +637,7 @@ value="<?php echo $Product['categoryID']; ?>">
 
 
 
-</section>
+<!--</section>
 <?php
 include('includes/footer.php');
-?>
+?>-->
